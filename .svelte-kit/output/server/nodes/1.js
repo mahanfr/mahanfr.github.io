4a436b133acf3f2bb616d7aed0cfa4f7f539199d
mahanfarzaneh2000 <mahan.farzaneh@gmail.com>
2023-06-29 21:04:08 +0330

    fixing deployment problems

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.5f5e8ae8.js","_app/immutable/chunks/index.5db5db0a.js","_app/immutable/chunks/singletons.6c0be38e.js"];
-export const stylesheets = [];
-export const fonts = [];
